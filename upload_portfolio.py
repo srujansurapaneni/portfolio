578f95947eef1a011592bb80b4f30d87e8c79b8b
srujansurapaneni <srujansurapaneni@gmail.com>
2020-07-14 22:31:10 -0400

    added codepipeline and event references

--- upload_portfolio.py
+++ upload_portfolio.py
@@ -23,12 +23,17 @@ def lambda_handler(event, context):
     topic = sns.Topic('arn:aws:sns:us-east-1:092781121214:PortfolioDeploy')
 
     try:
-        code_bucket = s3.Bucket('codebuild-portfolio')
+        job = event["CodePipeline.job"]["data"]["inputArtifacts"][0]["location"]["s3Location"]
+        print(job)
+
+        # code_bucket = s3.Bucket('codebuild-portfolio')
+        code_bucket = s3.Bucket(job["bucketName"])
+        object_key = job["objectKey"]
         project_bucket = s3.Bucket('portfolio.semantiqlabs.com')
 
         ram_stored_zip = StringIO.StringIO()
 
-        code_bucket.download_fileobj('CodeBuildPortfolio',ram_stored_zip)
+        code_bucket.download_fileobj(object_key,ram_stored_zip)
 
         with zipfile.ZipFile(ram_stored_zip) as myzip:
             for nm in myzip.namelist():
@@ -38,6 +43,9 @@ def lambda_handler(event, context):
                 project_bucket.Object(nm).Acl().put(ACL='public-read')
         topic.publish(Subject='Portfolio deployed', Message='Porfolio changes deployed successfully')
         print("deployed")
+
+        code_pipeline = boto3.client('codepipeline')
+        code_pipeline.put_job_success_result(jobId=event["CodePipeline.job"]["id"])
 
     except:
         topic.publish(Subject='Portfolio deployment failed', Message='Porfolio changes deployment failed, check the cloudwatch errors for more info')
